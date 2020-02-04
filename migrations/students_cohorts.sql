56e3f15c4107d21a5ef69080003b61623e1d088b
hiba02 <kangyoonsoon@hotmail.com>
2020-02-04 04:55:10 +0000

    BootcampX Queries 3 - Group By

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,4 +1,3 @@
-/*
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -15,22 +14,4 @@ CREATE TABLE students (
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
-);
-*/
-
-CREATE TABLE assignments (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  content TEXT,
-  day INTEGER,
-  chapter INTEGER,
-  duration INTEGER
-);
-
-CREATE TABLE assignment_submissions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
-  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration INTEGER,
-  submission_date DATE
 );
